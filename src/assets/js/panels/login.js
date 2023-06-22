9626cdf29c19a96fb5e79bc0b1a496afcafe991c
Watherfoxx <kubeur85@gmail.com>
2023-06-22 20:47:12 +0200

    Correctifs

--- src/assets/js/panels/login.js
+++ src/assets/js/panels/login.js
@@ -12,39 +12,39 @@ const { ipcRenderer } = require('electron');
 class Login {
     static id = "login";
     async init(config) {
-        this.config = config
+        this.config = config;
         this.database = await new database().init();
-        if (this.config.online) this.getOnline()
-        else this.getOffline()
+        if (this.config.online) this.getOnline();
+        else this.getOffline();
     }
 
     getOnline() {
-        console.log(`Initializing microsoft Panel...`)
-        console.log(`Initializing mojang Panel...`)
+        console.log(`Initializing microsoft Panel...`);
+        console.log(`Initializing mojang Panel...`);
         this.loginMicrosoft();
         this.loginMojang();
         document.querySelector('.cancel-login').addEventListener("click", () => {
             document.querySelector(".cancel-login").style.display = "none";
             changePanel("settings");
-        })
+        });
     }
 
     getOffline() {
-        console.log(`Initializing microsoft Panel...`)
-        console.log(`Initializing mojang Panel...`)
-        console.log(`Initializing offline Panel...`)
+        console.log(`Initializing microsoft Panel...`);
+        console.log(`Initializing mojang Panel...`);
+        console.log(`Initializing offline Panel...`);
         this.loginMicrosoft();
         this.loginOffline();
         document.querySelector('.cancel-login').addEventListener("click", () => {
             document.querySelector(".cancel-login").style.display = "none";
             changePanel("settings");
-        })
+        });
     }
 
     loginMicrosoft() {
-        let microsoftBtn = document.querySelector('.microsoft')
-        let mojangBtn = document.querySelector('.mojang')
-        let cancelBtn = document.querySelector('.cancel-login')
+        let microsoftBtn = document.querySelector('.microsoft');
+        let mojangBtn = document.querySelector('.mojang');
+        let cancelBtn = document.querySelector('.cancel-login');
 
         microsoftBtn.addEventListener("click", () => {
             microsoftBtn.disabled = true;
@@ -78,12 +78,12 @@ class Login {
                     capes: account_connect.profile.capes || []
                 }
 
-                this.database.add(account, 'accounts')
-                this.database.add(profile, 'profile')
+                this.database.add(account, 'accounts');
+                this.database.add(profile, 'profile');
                 this.database.update({ uuid: "1234", selected: account.uuid }, 'accounts-selected');
 
-                addAccount(account)
-                accountSelect(account.uuid)
+                addAccount(account);
+                accountSelect(account.uuid);
                 changePanel("home");
 
                 microsoftBtn.disabled = false;
@@ -91,99 +91,71 @@ class Login {
                 cancelBtn.disabled = false;
                 cancelBtn.style.display = "none";
             }).catch(err => {
-                console.log(err)
+                console.log(err);
                 microsoftBtn.disabled = false;
                 mojangBtn.disabled = false;
                 cancelBtn.disabled = false;
-
             });
-        })
+        });
     }
 
     loginMojang() {
-        let mailInput = document.querySelector('.Mail')
-        let passwordInput = document.querySelector('.Password')
-        let cancelMojangBtn = document.querySelector('.cancel-mojang')
-        let infoLogin = document.querySelector('.info-login')
-        let loginBtn = document.querySelector(".login-btn")
-        let mojangBtn = document.querySelector('.mojang')
+        let mailInput = document.querySelector('.Mail');
+        let cancelMojangBtn = document.querySelector('.cancel-mojang');
+        let infoLogin = document.querySelector('.info-login');
+        let loginBtn = document.querySelector(".login-btn");
+        let mojangBtn = document.querySelector('.mojang');
 
         mojangBtn.addEventListener("click", () => {
-            document.querySelector(".login-card").style.display = "none";
-            document.querySelector(".login-card-mojang").style.display = "block";
-        })
-
-        cancelMojangBtn.addEventListener("click", () => {
-            document.querySelector(".login-card").style.display = "block";
-            document.querySelector(".login-card-mojang").style.display = "none";
-        })
+            cancelMojangBtn.style.display = "block";
+            infoLogin.textContent = "";
+        });
 
         loginBtn.addEventListener("click", () => {
-            cancelMojangBtn.disabled = true;
-            loginBtn.disabled = true;
-            mailInput.disabled = true;
-            passwordInput.disabled = true;
-            infoLogin.innerHTML = "Connexion en cours...";
-
-
-            if (mailInput.value == "") {
-                infoLogin.innerHTML = "Entrez votre adresse email / Nom d'utilisateur"
-                cancelMojangBtn.disabled = false;
-                loginBtn.disabled = false;
-                mailInput.disabled = false;
-                passwordInput.disabled = false;
-                return
+            if (mailInput.value.trim() === "") {
+                infoLogin.textContent = "Veuillez saisir une adresse e-mail ou un nom d'utilisateur.";
+                return;
             }
 
-            if (passwordInput.value == "") {
-                infoLogin.innerHTML = "Entrez votre mot de passe"
-                cancelMojangBtn.disabled = false;
-                loginBtn.disabled = false;
-                mailInput.disabled = false;
-                passwordInput.disabled = false;
-                return
-            }
-
-            let account_connect = Mojang.login(mailInput.value, passwordInput.value)
-
-            if(account_connect == null || account_connect.error) {
-                console.log(err)
-                cancelMojangBtn.disabled = false;
-                loginBtn.disabled = false;
-                mailInput.disabled = false;
-                passwordInput.disabled = false;
-                infoLogin.innerHTML = 'Adresse E-mail ou mot de passe invalide'
-                return
-            }
-
-            let account = {
-                access_token: account_connect.access_token,
-                client_token: account_connect.client_token,
-                uuid: account_connect.uuid,
-                name: account_connect.name,
-                user_properties: account_connect.user_properties,
-                meta: {
-                    type: account_connect.meta.type,
-                    offline: account_connect.meta.offline
+            let mojang = new Mojang();
+            mojang.validate(mailInput.value.trim()).then(async (response) => {
+                if (!response.valid) {
+                    infoLogin.textContent = "Adresse e-mail ou nom d'utilisateur invalide.";
+                    return;
                 }
-            }
 
-            this.database.add(account, 'accounts')
-            this.database.update({ uuid: "1234", selected: account.uuid }, 'accounts-selected');
+                mojang.authenticate(mailInput.value.trim(), response.type === "email" ? "email" : "username").then(account => {
+                    let profile = {
+                        uuid: account.uuid,
+                        skins: [],
+                        capes: []
+                    }
 
-            addAccount(account)
-            accountSelect(account.uuid)
-            changePanel("home");
+                    this.database.add(account, 'accounts');
+                    this.database.add(profile, 'profile');
+                    this.database.update({ uuid: "1234", selected: account.uuid }, 'accounts-selected');
 
-            cancelMojangBtn.disabled = false;
-            cancelMojangBtn.click();
+                    addAccount(account);
+                    accountSelect(account.uuid);
+                    changePanel("home");
+
+                    cancelMojangBtn.style.display = "none";
+                    infoLogin.textContent = "";
+                }).catch(err => {
+                    console.log(err);
+                    infoLogin.textContent = "Échec de l'authentification Mojang.";
+                });
+            }).catch(err => {
+                console.log(err);
+                infoLogin.textContent = "Erreur lors de la validation de l'adresse e-mail ou du nom d'utilisateur.";
+            });
+        });
+
+        cancelMojangBtn.addEventListener("click", () => {
             mailInput.value = "";
-            loginBtn.disabled = false;
-            mailInput.disabled = false;
-            passwordInput.disabled = false;
-            loginBtn.style.display = "block";
-            infoLogin.innerHTML = "&nbsp;";
-        })
+            cancelMojangBtn.style.display = "none";
+            infoLogin.textContent = "";
+        });
     }
 
     loginOffline() {
@@ -208,8 +180,7 @@ class Login {
             cancelMojangBtn.disabled = true;
             loginBtn.disabled = true;
             mailInput.disabled = true;
-            passwordInput.disabled = true;
-            infoLogin.innerHTML = "Connexion en cours...";
+            // infoLogin.innerHTML = "Connexion en cours...";
 
 
             if (mailInput.value == "") {
@@ -226,11 +197,10 @@ class Login {
                 cancelMojangBtn.disabled = false;
                 loginBtn.disabled = false;
                 mailInput.disabled = false;
-                passwordInput.disabled = false;
                 return
             }
 
-            let account_connect = Mojang.login(mailInput.value, passwordInput.value)
+            let account_connect = Mojang.login(mailInput.value)
 
             if(account_connect == null || account_connect.error) {
                 console.log(err)
@@ -266,7 +236,7 @@ class Login {
             mailInput.value = "";
             loginBtn.disabled = false;
             mailInput.disabled = false;
-            passwordInput.disabled = false;
+            
             loginBtn.style.display = "block";
             infoLogin.innerHTML = "&nbsp;";
         })
