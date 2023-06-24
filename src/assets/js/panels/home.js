1ed5f81a48f6b8bcc79458235b03132811b8364d
Watherfoxx <kubeur85@gmail.com>
2023-06-24 20:33:45 +0200

    Update home.js

--- src/assets/js/panels/home.js
+++ src/assets/js/panels/home.js
@@ -169,12 +169,14 @@ class Home {
             })
 
             launch.on('patch', patch => {
+                console.log(patch);
                 info.innerHTML = `Remplissage des mers...`
             });
 
             launch.on('data', (e) => {
                 new logger('Minecraft', '#36b030');
                 if (launcherSettings.launcher.close === 'close-launcher') ipcRenderer.send("main-window-hide");
+                ipcRenderer.send('main-window-progress-reset')
                 progressBar.style.display = "none"
                 info.innerHTML = `Production du grog...`
                 console.log(e);
@@ -185,7 +187,7 @@ class Home {
                 progressBar.style.display = "none"
                 info.style.display = "none"
                 playBtn.style.display = "block"
-                info.innerHTML = `Nettoyage des canons...`
+                info.innerHTML = `Nettoyage du pont...`
                 new logger('Launcher', '#7289da');
                 console.log('Close');
             });
